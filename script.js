cc38aa9bf31b5e1f8d0c3ec91a832629db63e2ed
Madison Vega <madison.r.vega@gmail.com>
2021-02-07 21:04:58 -0600

    added variables for password criteria

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -11,5 +13,74 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); {
 
+}
+
+function generatePassword() {
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "1234567890";
+  var symbols = "!@#$%^&*()";
+  var length = prompt("Input length of password between 1-128");
+  var lower = confirm("Include lowercase?");
+  var upper = confirm("Include uppercase?");
+  var numbers = confirm("Include numbers?");
+  var symbols = confirm("Include symbols?");
+  var types = 0;
+
+
+  if (lower) {
+   var password =+ lowerCase.charAt(Math.floor(Math.random() 
+    * lowerCase.length + 1)); 
+    types++;
+  
+  }
+  if (upper) {
+    var password =+ upperCase.charAt(Math.floor(Math.random() 
+     * upperCase.length + 1)); 
+     types++;
+   
+   }
+   if (numbers) {
+    var password =+ numbers.charAt(Math.floor(Math.random() 
+     * numbers.length + 1)); 
+     types++;
+   
+   }
+   if (symbols) {
+    var password =+ symbols.charAt(Math.floor(Math.random() 
+     * symbols.length + 1)); 
+     types++;
+   
+   }
+
+   var characterNeed = length - password.length;
+
+   for (i = 0; i < characterNeed; i++) {
+     var randomNumber = (Math.floor(Math.random() * types + 1));
+     if (randomNumber == 0) {
+
+     }
+     if (randomNumber == 1) {
+       
+    }
+    if (randomNumber == 2) {
+       
+    }
+    if (randomNumber == 3) {
+       
+    }
+     
+   }
+
+}
+
+
+
+
+
+  // click generate password
+  // box prompt- enter length password 
+  // box prompt to select yes for parameters
+  // password should generate 
